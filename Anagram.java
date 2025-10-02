702f8bf923c7466bf88dec2ea338a7d48327e863
saijasmine <128382944+saijasmine@users.noreply.github.com>
2025-10-02 20:42:34 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,8 +1,8 @@
 import java.util.*;
-class Anagram {
+class Main {
     public static void main(String[] args) {
-        String str1= "jasmine";
-        String str2="minejs";
+        String str1= "jasmine sai";
+        String str2="minejsasai";
         
         boolean anagram=checkAnagram(str1,str2);
         
@@ -14,6 +14,8 @@ class Anagram {
     }
     
     public static boolean checkAnagram(String str1,String str2){
+        str1=str1.replaceAll("\\s","").toLowerCase();
+        str2=str2.replaceAll("\\s","").toLowerCase();
         if(str1.length()!=str2.length()){
             return false;
         }
