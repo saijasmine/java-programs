cb68d4efe771f958085607f1e0aa67fdeb19bc51
saijasmine <128382944+saijasmine@users.noreply.github.com>
2025-10-02 22:25:19 +0530

    Create ArrayMinMax.java

---
+++ ArrayMinMax.java
@@ -0,0 +1,20 @@
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr={12,32,13,2,7};
+        
+        int max=arr[0];
+        int min=arr[0];
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
