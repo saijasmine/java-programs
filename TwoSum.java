d4d66dc2bd661dea3627918cdf03e557dae87c09
saijasmine <128382944+saijasmine@users.noreply.github.com>
2025-10-02 22:52:49 +0530

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+    public static int[] twosum(int[] arr, int target){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        int target=6;
+        int[] result= twosum(arr,target);
+        System.out.println(Arrays.toString(result));
+    }
+}
